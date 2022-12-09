b91dece3efd99de49bcc9a96bbc8667278ac09d8
Hope <drisenhope@gmail.com>
2022-12-08 17:00:55 -0800

    Task Completed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -10,10 +9,10 @@
 int main(void)
 
 {
-	printf("Size of a char:=%i bytes\n, ");
-	printf("Size of a int:=%i bytes\n, ");
-	printf("Size of a long int:=%i bytes\n, ");
-	printf("Size of a long long int:=%i bytes\n, ");
-	printf("Size of a float:=%i bytes\n, ");
+	printf("Size of a char:=%i byts\n, ");
+	printf("Size of a int:=%i byte\n, ");
+	printf("Size of a long int:=%i byte\n, ");
+	printf("Size of a long long int:=%i byte\n, ");
+	printf("Size of a float:=%i byte\n, ");
 	return (0);
 }
